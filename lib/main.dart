d003addab216c9a837aff8735308c3564d146345
faguanil <faguanil@gmail.com>
2025-04-14 17:03:26 +0000

    Novo projeto flutter-techtaste

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/ui/_core/app_theme.dart';
+import 'package:myapp/ui/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.appTheme,
+      home: SplashScreen(),
+    );
+  }
+}
+
+//primeira palavra chave:ALURA
